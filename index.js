93d316685f16ec44d13bd6f56a8b75a7f32a1bbb
Brian Luft <brian@lincolnloop.com>
2015-05-27 09:17:58 -0700

    CORS for easier dev; don't use in production

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+
+//TODO
+// for testing only
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/logs/:app', function(req, res) {
   var session = logSession(API_KEY);
